5429a21c99e1db0aa1bdadbcb284f16e7244d9e7
Christopher Stolfa <xcstolfax@gmail.com>
2022-11-13 18:32:39 -0700

    Fix hash map

--- src/components/Buttons/IconButton.tsx
+++ src/components/Buttons/IconButton.tsx
@@ -4,24 +4,25 @@ import { mail } from '../../assets/svgs';
 
 type IconVariant = 'facebook' | 'instagram' | 'linkedin' | 'mail' | 'twitter' | 'youtube';
 
-const iconVariants = [
-  { facebook: facebook },
-  { instagram: instagram },
-  { linkedin: linkedin },
-  { twitter: twitter },
-  { youtube: youtube },
-  { mail: mail },
-];
-
+const iconVariants = {
+  facebook: facebook,
+  instagram: instagram,
+  linkedin: linkedin,
+  twitter: twitter,
+  youtube: youtube,
+  mail: mail,
+};
 interface Props {
   href: string;
   variant: IconVariant;
 }
 
 const IconButton = ({ href, variant }: Props) => {
-  <a href={href}>
-    <img alt={variant} src={iconVariants[variant]} />
-  </a>;
+  return (
+    <a href={href}>
+      <img alt={variant} src={iconVariants[variant]} />
+    </a>
+  );
 };
 
 export default IconButton;
