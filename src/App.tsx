02ef950f6e201c9484d40af027271c5078c2d3f3
Christopher Stolfa <xcstolfax@gmail.com>
2022-11-18 23:13:02 -0700

    Add tertiary button variant and create a mixin for border

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const App = () => (
   <>
     <Button variant="primary">primary</Button>
     <Button variant="secondary">secondary</Button>
+    <Button variant="tertiary">tertiary</Button>
     <Card
       date="9/9/1999"
       title="It's a Card!"
