57c1ef9772b39466b7df0f47047cb2d474df2e50
Christopher Stolfa <xcstolfax@gmail.com>
2022-12-04 20:21:34 -0700

    Add PageDataContext, att GET_DATA query, when App mounts request wordpress data and set it to context. Use the context in CreativeSpaces to set the data for that widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import styles from './App.module.scss';
 import { Button, IconButton } from './components/Buttons';
 import { Card } from './components/Cards';
@@ -6,31 +6,45 @@ import { Hero } from './components/Hero';
 import Tabs from './components/Tabs/Tabs';
 import { Tab, TabList, TabPanel, TabPanels } from './components/Tabs';
 import { CreativeSpaces } from './sections';
+import GET_PAGE_DATA, { IPageData } from './data/get-data';
+import { useQuery } from '@apollo/client';
+import { PageDataProvider } from './contexts/PageDataContext';
 /**
  * This is the root React component.
  *
  * @component
  */
 
-const App = () => (
-  <div className={styles['root']}>
-    <div>
-      <Hero />
-    </div>
-    <CreativeSpaces />
+const App = () => {
+  const [pageData, setPageData] = useState<IPageData>({});
+  const { data, loading, error } = useQuery<IPageData>(GET_PAGE_DATA);
 
-    <Button variant="primary">primary</Button>
-    <Button variant="secondary">secondary</Button>
-    <Button variant="tertiary">tertiary</Button>
-    <Card
-      date="9/9/1999"
-      title="It's a Card!"
-      body="In this string is a body of text."
-      personName="John Doe"
-      label="Professuh"
-    />
-    <IconButton href="facebook.com" variant="facebook" />
-  </div>
-);
+  useLayoutEffect(() => {
+    if (data) setPageData(data);
+  }, [data, loading, error]);
+
+  return (
+    <PageDataProvider value={{ pageData }}>
+      <div className={styles['root']}>
+        <div>
+          <Hero />
+        </div>
+        <CreativeSpaces />
+
+        <Button variant="primary">primary</Button>
+        <Button variant="secondary">secondary</Button>
+        <Button variant="tertiary">tertiary</Button>
+        <Card
+          date="9/9/1999"
+          title="It's a Card!"
+          body="In this string is a body of text."
+          personName="John Doe"
+          label="Professuh"
+        />
+        <IconButton href="facebook.com" variant="facebook" />
+      </div>
+    </PageDataProvider>
+  );
+};
 
 export default App;
--- src/sections/CreativeSpaces.tsx
+++ src/sections/CreativeSpaces.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import styles from './CreativeSpaces.module.scss';
 import indexStyles from './index.module.scss';
 import { Tab, TabArrows, TabList, TabPanel, TabPanels } from '../components/Tabs';
@@ -9,20 +9,17 @@ import { uniqueId } from 'lodash';
 import Img from 'react-cool-img';
 import { placeholderImg } from '../assets';
 import { Button } from '../components/Buttons';
-import GET_CREATIVE_SPACES, { ICreativeSpacesData } from '../data/get-creative-spaces';
-import { useQuery } from '@apollo/client';
 import { regexStripHtml } from '../utils';
+import { PageDataContext } from '../contexts/PageDataContext';
 
 /**
  * Creative Spaces section with tablist content
  * @component
  */
 const CreativeSpaces = () => {
-  const { data, loading, error } = useQuery<ICreativeSpacesData>(GET_CREATIVE_SPACES);
-
-  React.useEffect(() => {
-    console.log(data);
-  }, [data]);
+  const {
+    pageData: { page },
+  } = useContext(PageDataContext);
 
   return (
     <Container>
@@ -31,26 +28,26 @@ const CreativeSpaces = () => {
         <Tabs className={styles['tabs-container']}>
           <div className={styles['tabs-flex']}>
             <TabList className={styles['tablist']}>
-              {data?.page?.landingPage?.creativespaces?.map(({ title }, i) => (
+              {page?.landingPage?.creativeSpaces?.map(({ title }, i) => (
                 <Tab key={uniqueId(title)} id={i}>
                   {title}
                 </Tab>
               ))}
             </TabList>
             <TabPanels>
-              {data?.page?.landingPage?.creativespaces?.map(
-                ({ title, link, altText, description }, i) => (
+              {page?.landingPage?.creativeSpaces?.map(
+                ({ title, description, image, moreInfoUrl, schedule }, i) => (
                   <TabPanel key={uniqueId(title)} id={i}>
                     <span className={styles['small-screen-header']}>
                       <Header as="h3">{title}</Header>
-                      <TabArrows size={data?.page?.landingPage?.creativespaces?.length} />
+                      <TabArrows size={page?.landingPage?.creativeSpaces?.length || 0} />
                     </span>
                     <Img
                       lazy
                       cache
-                      src={link}
+                      src={image?.link}
                       placeholder={placeholderImg}
-                      alt={altText}
+                      alt={image?.altText}
                       sizes="(max-width: 648px) 100vw, 648px"
                       width="100%"
                       height="auto"
