0a05176ea6f461c1db119c21733980fad69a6ef3
Christopher Stolfa <xcstolfax@gmail.com>
2022-12-18 19:18:37 -0700

    fix paths

---
+++ src/sections/SpitzerOath/SpitzerOath.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SpitzerOath = () => <>yada yada yada</>;
+
+export default SpitzerOath;
--- src/sections/index.ts
+++ src/sections/index.ts
@@ -3,3 +3,4 @@ export { default as HeroCarousel } from './HeroCarousel/HeroCarousel';
 export { default as Jedi } from './Jedi/Jedi';
 export { default as Programs } from './Programs/Programs';
 export { default as NewsAndEvents } from './NewsAndEvents/NewsAndEvents';
+export { default as SpitzerOath } from './SpitzerOath/SpitzerOath';
