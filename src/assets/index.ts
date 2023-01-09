5d325e4c2514488181f04f4e5c8ee42b53d2b4ae
Christopher Stolfa <xcstolfax@gmail.com>
2023-01-08 20:34:10 -0700

    add placeholder blackbox png

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,3 @@
 export { default as placeholderImg } from './placeholder-image.png';
 export { default as digitourThumbnail } from './webp/digitourThumbnail.webp';
+export { default as blackBox } from './pngs/black-box.png';
