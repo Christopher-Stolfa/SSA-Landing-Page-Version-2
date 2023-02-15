70a7034c03a9f66001be9159778ec776e7e41f45
Christopher Stolfa <xcstolfax@gmail.com>
2023-02-14 20:33:35 -0700

    Add base case

--- src/utils/regexStripHtml.ts
+++ src/utils/regexStripHtml.ts
@@ -1,10 +1,12 @@
 const regexStripHtml = (string = '') => {
+  if (!string) return '';
+  if (typeof string !== 'string') return '';
   let newString = string;
   const stripHtml = /(<([^>]+)>)/gi;
   const stripEntities = /&#{0,1}[a-z0-9]+;/gi;
 
-  newString = newString.replace(stripHtml, '');
-  newString = newString.replace(stripEntities, '');
+  newString = newString?.replace(stripHtml, '');
+  newString = newString?.replace(stripEntities, '');
   return newString;
 };
 
